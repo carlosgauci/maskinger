c81cfae05564b4f29710c9baac657a12be605997
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-05-29 11:25:51 +0200

    add link to header title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from "react"
 import { BiMenu, BiCartAlt } from "react-icons/bi"
+import { Link } from "gatsby"
 
 const Header = () => {
   return (
     <header className="fixed w-full h-20 bg-white border-b border-gray-200 z-10">
       <div className="container h-full flex justify-between items-center">
         {/* Title */}
-        <h1 className="text-4xl">Maskinger</h1>
+        <Link to="/">
+          <h1 className="text-4xl">Maskinger</h1>
+        </Link>
 
         {/* Nav & icons */}
         <nav className="flex gap-4 md:gap-8">
