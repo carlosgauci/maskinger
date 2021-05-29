eb014bdb9d467af7a9c52449dccdd42cc27a8504
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-05-29 23:37:48 +0200

    update readme, 404 styling

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Maskinger
+
+An ecommerce website built with Gatsby, Redux, Airtable, Stripe & Netlify functions.
+
+https://maskinger.com
--- src/pages/404.js
+++ src/pages/404.js
@@ -3,9 +3,11 @@ import { Link } from "gatsby"
 import Button from "../components/Button"
 
 const NotFoundPage = () => (
-  <div className="container py-28 flex flex-col justify-center">
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <div className="container py-28 flex flex-col items-center">
+    <h1 className="text-3xl md:text-5xl mb-6">404: Not Found</h1>
+    <p className="mb-6">
+      You just hit a route that doesn&#39;t exist... the sadness.
+    </p>
     <Link to="/">
       <Button text="GO BACK" fullWidth={false} />
     </Link>
