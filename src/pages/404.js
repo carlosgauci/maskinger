f15193c0b367e917af4f52b4e176ffdd7e9962ac
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-05-29 23:22:43 +0200

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
+import Button from "../components/Button"
 
 const NotFoundPage = () => (
-  <div>
+  <div className="container py-28 flex flex-col justify-center">
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Link to="/">
+      <Button text="GO BACK" fullWidth={false} />
+    </Link>
   </div>
 )
 
