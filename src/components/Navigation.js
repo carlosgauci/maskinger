ad88afa56c8f91d8998a4f3ebdd6b1aea1634bb0
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-05-30 11:08:52 +0200

    update navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -16,13 +16,33 @@ export default function Navigation() {
       animate="enter"
       exit="exit"
     >
-      <h3 className="font-heading text-3xl font-normal mb-4">Navigation</h3>
+      <h3 className="font-heading text-3xl mb-4">Navigation</h3>
 
+      {/* Shop */}
       <AnchorLink to={`/#shop`} onAnchorLinkClick={() => dispatch(toggleNav())}>
-        <button className="text-2xl font-normal">Shop</button>
+        <button className="text-2xl font-heading font-normal">Shop</button>
       </AnchorLink>
+
+      {/* Cart */}
       <Link to="/cart" onClick={() => dispatch(toggleNav())}>
-        <button className="text-2xl font-normal">Cart</button>
+        <button className="text-2xl font-heading font-normal">Cart</button>
+      </Link>
+
+      {/* Terms, Privacy, Shipping */}
+      <Link to="/shipping" onClick={() => dispatch(toggleNav())}>
+        <button className="text-2xl font-heading font-normal">Shipping</button>
+      </Link>
+
+      <Link to="/terms-of-service" onClick={() => dispatch(toggleNav())}>
+        <button className="text-2xl font-heading font-normal">
+          Terms of Service
+        </button>
+      </Link>
+
+      <Link to="/privacy-policy" onClick={() => dispatch(toggleNav())}>
+        <button className="text-2xl font-heading font-normal">
+          Privacy Policy
+        </button>
       </Link>
     </motion.nav>
   )
