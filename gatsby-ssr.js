41f1426d136ccca833b9fabcfce1ef0c1f78c88a
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-05-30 11:41:37 +0200

    update gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,21 @@
 import React from "react"
 import Layout from "./src/components/Layout"
 import "./src/styles/global.css"
-import { createStore, compose } from "redux"
+import { createStore } from "redux"
 import { Provider } from "react-redux"
 import { cartReducer } from "./src/reducers/cartReducer"
+import { loadState, saveState } from "./src/utils/localStorage"
+
+// Get state from local storage
+const persistedState = loadState()
 
 // Redux store
-const store = createStore(cartReducer)
+const store = createStore(cartReducer, persistedState)
+
+// Save to local storage on store change
+store.subscribe(() => {
+  saveState(store.getState())
+})
 
 export const wrapPageElement = ({ element, props }) => {
   return (
